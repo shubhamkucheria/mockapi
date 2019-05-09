7650cdaa56c95415f2291fa1a27624d123fb6fd7
Shubham Kucheria <Shubhamkucheria@gmail.com>
2019-05-09 16:23:40 +0530

    Delete res.js

--- res.js
+++
@@ -1,6 +0,0 @@
-Zycus is a leading global provider of complete Source-to-Pay suite of procurement performance solutions.
- 
-
- Implement client-side code using jQuery, AJAX or other libraries.
-- Implementation of security, session management, localization & other non-functional needs
-- Evaluate tools/frameworks & create PoCs as required.
